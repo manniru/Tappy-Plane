f8a2d07a5fc06d2881800c66bf5ad010b964cec2
Jed Hastwell <jed@whistlefire.com>
2014-04-14 19:14:27 +0200

    Add mountain graphics and create TPObstacleLayer class

---
+++ Tappy Plane/TPObstacleLayer.h
@@ -0,0 +1,13 @@
+//
+//  TPObstacleLayer.h
+//  Tappy Plane
+//
+//  Created by J Hastwell on 28/03/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "TPScrollingNode.h"
+
+@interface TPObstacleLayer : TPScrollingNode
+
+@end
---
+++ Tappy Plane/TPObstacleLayer.m
@@ -0,0 +1,33 @@
+//
+//  TPObstacleLayer.m
+//  Tappy Plane
+//
+//  Created by J Hastwell on 28/03/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "TPObstacleLayer.h"
+
+@implementation TPObstacleLayer
+
+
+-(void)updateWithTimeElpased:(NSTimeInterval)timeElapsed
+{
+    [super updateWithTimeElpased:timeElapsed];
+    
+}
+
+
+
+
+@end
+
+
+
+
+
+
+
+
+
+
