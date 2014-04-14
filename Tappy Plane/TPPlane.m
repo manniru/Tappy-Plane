e92af79c6555567a61d4620c02d630fd1f9f2427
Jed Hastwell <jed@whistlefire.com>
2014-04-14 19:09:00 +0200

    Set target node on plane's puff trial emitter

--- Tappy Plane/TPPlane.m
+++ Tappy Plane/TPPlane.m
@@ -55,6 +55,7 @@ static NSString* const kKeyPlaneAnimation = @"PlaneAnimation";
 {
     _engineRunning = engineRunning;
     if (engineRunning) {
+        self.puffTrailEmitter.targetNode = self.parent;
         [self actionForKey:kKeyPlaneAnimation].speed = 1;
         self.puffTrailEmitter.particleBirthRate = self.puffTrailBirthRate;
     }
