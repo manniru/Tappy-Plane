cdfea1646b8780e3c7db14449f192cfdb26c8a56
Jed Hastwell <jed@whistlefire.com>
2014-04-14 18:59:08 +0200

    Initial commit

---
+++ Tappy Plane/TPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPAppDelegate.h
+//  Tappy Plane
+//
+//  Created by J Hastwell on 20/03/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tappy Plane/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tappy Plane
+//
+//  Created by J Hastwell on 20/03/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPAppDelegate class]));
+    }
+}
