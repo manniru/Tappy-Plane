bea114a3b73a46f50964d9a2fd72595cc1e214a3
Jed Hastwell <jed@whistlefire.com>
2014-05-02 17:15:02 +0200

    Add graphics and plist for different tile sets

---
+++ Tappy Plane/TPTilesetTextureProvider.h
@@ -0,0 +1,15 @@
+//
+//  TPTilesetTextureProvider.h
+//  Tappy Plane
+//
+//  Created by J Hastwell on 2/05/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPTilesetTextureProvider : NSObject
+
++(instancetype)getProvider;
+
+@end
---
+++ Tappy Plane/TPTilesetTextureProvider.m
@@ -0,0 +1,63 @@
+//
+//  TPTilesetTextureProvider.m
+//  Tappy Plane
+//
+//  Created by J Hastwell on 2/05/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "TPTilesetTextureProvider.h"
+#import <SpriteKit/SpriteKit.h>
+
+@interface TPTilesetTextureProvider()
+
+@property (nonatomic) NSMutableDictionary *tilesets;
+
+@end
+
+@implementation TPTilesetTextureProvider
+
+
++(instancetype)getProvider
+{
+    static TPTilesetTextureProvider* provider = nil;
+    @synchronized(self) {
+        if (!provider) {
+            provider = [[TPTilesetTextureProvider alloc] init];
+        }
+        return provider;
+    }
+}
+
+-(void)loadTilesets
+{
+    self.tilesets = [[NSMutableDictionary alloc] init];
+    SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:@"Graphics"];
+    
+    // Get path to property list.
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"TilesetGraphics" ofType:@"plist"];
+    // Load contents of file.
+    NSDictionary *tilesetList = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    // Loop through tilesetList.
+    for (NSString *tilesetKey in tilesetList) {
+        // Get dictionary of texture names.
+        NSDictionary *textureList = [tilesetList objectForKey:tilesetKey];
+        // Create dictionary to hold textures.
+        NSMutableDictionary *textures = [[NSMutableDictionary alloc] init];
+        
+        for (NSString *textureKey in textureList) {
+            // Get texture for key.
+            SKTexture *texture = [atlas textureNamed:[textureList objectForKey:textureKey]];
+            // Insert texture to textures dictionary.
+            [textures setObject:texture forKey:textureKey];
+        }
+        
+        // Add textures dictionary to tilesets.
+        [self.tilesets setObject:textures forKey:tilesetKey];
+    }
+    
+}
+
+
+
+@end
